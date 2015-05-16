92b4040790b75aec5d98461173b7f2d857b9f007
Nick Schwaderer <nicholas.schwaderer@gmail.com>
2015-05-16 13:21:25 -0600

    removes user model and controller for devise implementation

--- db/migrate/20150515173555_create_users.rb
+++
@@ -1,9 +0,0 @@
-class CreateUsers < ActiveRecord::Migration
-  def change
-    create_table :users do |t|
-      t.string :email
-      t.integer :hit_count
-      t.timestamps null: false
-    end
-  end
-end
