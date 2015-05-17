c7e03671b165f64e3eb02dad41825fb7244c98b8
Nick Schwaderer <nicholas.schwaderer@gmail.com>
2015-05-17 11:40:54 -0600

    renders index and show pages for queries

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   def credit_card_search(query)
     sanitize_query(query)
     client = SODA::Client.new({:domain => "data.datamontana.us", :app_token => "lHUM5m1RF3QfiupyzYsQQSDrY"})
-    return client.get("dvui-wnnh", {:payee => "#{query}"})
+    return client.get("dvui-wnnh", {:merchant => "#{query}"})
   end
 
   def accts_payable_search(query)
