6fd7707f3f9faa346de4eec5ba3ebd8d8604a052
Nick Schwaderer <nicholas.schwaderer@gmail.com>
2015-05-17 07:13:56 -0600

    configures final changes to reimplement devise

--- db/migrate/20150515174342_add_foreign_key_to_query.rb
+++
@@ -1,5 +0,0 @@
-class AddForeignKeyToQuery < ActiveRecord::Migration
-  def change
-    add_column :queries, :user_id, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -16,18 +16,6 @@ ActiveRecord::Schema.define(version: 20150515174342) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "queries", force: :cascade do |t|
-    t.string   "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer  "user_id"
-  end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "email"
-    t.integer  "hit_count"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
 end
