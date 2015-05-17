e10a7973974e0cd95c5613fa6eb9b5d97a682d2c
Nick Schwaderer <nicholas.schwaderer@gmail.com>
2015-05-17 10:41:30 -0600

    creates methods for querying Montana State API

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,20 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  client = SODA::Client.new({:domain => "data.datamontana.us", :app_token => "lHUM5m1RF3QfiupyzYsQQSDrY"})
+  @credit_cards
+
+  def credit_card_search(query)
+    query.sanitize_query
+    return client.get("dvui-wnnh", {:payee => "#{query}"})
+  end
+
+  def accts_payable_search(query)
+ query.sanitize_query
+    return client.get("pjb4-8ve5", {:payee => "#{query}"})
+  end
+
+  def sanitize_query
+    self = self.upcase
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150517162343) do
+ActiveRecord::Schema.define(version: 20150517163421) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,16 @@ ActiveRecord::Schema.define(version: 20150517162343) do
     t.integer  "popularity"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+  end
+
+  create_table "credit_cards", force: :cascade do |t|
+    t.float    "amount"
+    t.integer  "fiscal_year"
+    t.datetime "billing_date"
+    t.string   "department"
+    t.string   "merchant"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
   create_table "queries", force: :cascade do |t|
