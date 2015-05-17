2d36dd1bc14a23fbb8a480038e280adb815d7a7f
Nick Schwaderer <nicholas.schwaderer@gmail.com>
2015-05-17 11:01:03 -0600

    updates many to many associations from queries to checks nad credit cards

--- app/models/check.rb
+++ app/models/check.rb
@@ -1,3 +1,3 @@
 class Check < ActiveRecord::Base
-  client = SODA::Client.new({:domain => "data.datamontana.us", :app_token => "lHUM5m1RF3QfiupyzYsQQSDrY"})
+  has_many :queries
 end
--- app/models/query.rb
+++ app/models/query.rb
@@ -1,3 +1,5 @@
 class Query < ActiveRecord::Base
   belongs_to :user
+  has_many :checks
+  has_many :credit_cards
 end
