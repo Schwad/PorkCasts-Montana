0382e910fc18d3fbbda58ebd13a3ac20fd932370
Nick Schwaderer <nicholas.schwaderer@gmail.com>
2015-05-17 17:02:36 -0600

    fixes paths for more robust query editing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,14 @@
 class UsersController < ApplicationController
   def update
-    @query = Query.new(
-      :content => params[:query][:content],
-      :user_id => current_user.id
-    )
+    if session[:editing_query]
+      @query = Query.find(session[:editing_query])
+      @query.content = params[:query][:content]
+    else
+      @query = Query.new(
+        :content => params[:query][:content],
+        :user_id => current_user.id
+      )
+    end
     if @query.save
       redirect_to user_queries_path(current_user.id)
     end
