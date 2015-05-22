110536c4f9b6d43ef2745340c91cfa3622c2e2f5
Nick Schwaderer <nicholas.schwaderer@gmail.com>
2015-05-22 12:47:35 -0600

    fixes bug to re-allow complete query creation upon front page first time sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,46 +18,6 @@ class ApplicationController < ActionController::Base
      return client.get("pjb4-8ve5", {:payee => "#{query}"})
   end
 
-#FINISH EMAIL METHODS, THEN CHANGE THE SHOW PAGE TO ONLY DO QUERY.CHECKS ETC, AND THIS API METHOD ONLY RUNS ONCE A DAY.
-
-  # def new_check_email(query)
-  #   checks = accts_payable_search(query)
-  #   checks.each do |check|
-  #     if query.checks.include?(check)
-
-  #     else
-  #       Check.create(
-  #         :query_id => query.id,
-  #         :payment_category => ,
-  #         :department => ,
-  #         :amount => ,
-  #         :payee => ,
-  #         :payment_date
-  #         )
-  #     end
-  #   end
-  # end
-
-  # def new_credit_card_check_email(query)
-  #   credit_cards = credit_card_search(query)
-  #   credit_cards.each do |credit_card|
-  #     if query.credit_cards.include?(credit_card)
-
-  #     else
-  #       CreditCard.create(
-  #         :query_id => query.id,
-  #         :payment_category => ,
-  #         :department => ,
-  #         :amount => ,
-  #         :merchant => ,
-  #         :billing_date
-  #         )
-  #     end
-  #   end
-
-
-  # end
-
   def sanitize_query(query)
     query.content.upcase
   end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,9 @@
 class StaticPagesController < ApplicationController
 
   def index
+    @new_query = nil
     if session[:new_user_query]
-      Query.create(
+      @new_query = Query.create(
           :content => session[:new_user_query],
           :user_id => current_user.id
         )
@@ -11,7 +12,38 @@ class StaticPagesController < ApplicationController
     @user = User.new
     @query = Query.new
     # @user = User.new
+    if @new_query
+      creates_checks(@new_query)
+      creates_credit_cards(@new_query)
+    end
   end
 
+  #need in helper method
+  def creates_checks(query)
+    @checks = accts_payable_search(query)
+    @checks.each do |check|
+      Check.create(
+          :query_id => query.id,
+          :payment_category => check.payment_category,
+          :department => check.department,
+          :amount => check.amount,
+          :payee => check.payee,
+          :payment_date => check.payment_date
+        )
+    end
+  end
+
+  def creates_credit_cards(query)
+    @credit_cards = credit_card_search(query)
+    @credit_cards.each do |credit_card|
+      CreditCard.create(
+        :query_id => query.id,
+        :department => credit_card.department,
+        :amount => credit_card.amount,
+        :merchant => credit_card.merchant,
+        :billing_date => credit_card.billing_date
+        )
+    end
+  end
 end
 
