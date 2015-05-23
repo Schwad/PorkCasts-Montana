a5de29de8c64c9c490490596bf794783e68ab219
Nick Schwaderer <nicholas.schwaderer@gmail.com>
2015-05-23 10:45:00 -0600

    passes correctly instance variables from mailer controller to the views

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -8,14 +8,20 @@ class UserMailer < ApplicationMailer
   end
 
   def query_match(query)
+    @query = query
+    @user = @query.user
     mail(to: query.user.email, subject: 'Check written to #{query.content}')
   end
 
   def test_mailer(user)
+    @user = user
     mail(to: user.email, subject: 'This is a test rake task in heroku scheduler to #{user.email}!')
   end
 
   def new_payment(user, payee, amount)
+    @user = user
+    @payee = payee
+    @amount = amount
     mail(to: user.email, subject: 'PORKCAST: New Payment to #{payee} from the State of Montana in the amount of $#{amount}')
   end
 end
