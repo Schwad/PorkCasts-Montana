11f917a9a7be0324f6d10ae1d869b88f25e5dbe8
Nick Schwaderer <nicholas.schwaderer@gmail.com>
2015-05-30 02:21:09 -0600

    improves listeners and implements sticky footer with active links

--- app/assets/javascripts/show.js
+++ app/assets/javascripts/show.js
@@ -1,52 +1,61 @@
 var myGraphPause = false;
 var myLinkPause = false;
 var myTablePause = false;
+var hoverEngage = false;
 
 $(document).ready(function(){
 
   // graph hover
   $('.graph-hold').hover(function(e) {
-    if(myGraphPause === false){
+    if(myGraphPause === false && hoverEngage === false){
       var x = e.pageX - 70;
       var y = e.pageY - 70;
-      $('.graph-popup').fadeIn(520, "swing").css({"margin-left": x, "margin-top": y});
       myGraphPause = true;
+      hoverEngage = true;
+      $('.graph-popup').fadeIn(520, "swing").css({"margin-left": x, "margin-top": y});
+
     }
   }, function() {
       setTimeout(function(){
       $('.graph-popup').fadeOut(830, "swing");
       }, 1000);
       setTimeout(function(){ myGraphPause = false; }, 30000);
+      setTimeout(function(){ hoverEngage = false; }, 1830);
   });
 
   // share hover
 
   $('.share-hold').hover(function(e) {
-    if(myLinkPause === false){
+    if(myLinkPause === false && hoverEngage === false){
       var x = e.pageX - 70;
       var y = e.pageY - 70;
-      $('.share-link').fadeIn(520, "swing").css({"margin-left": x, "margin-top": y});
       myLinkPause = true;
+      hoverEngage = true;
+      $('.share-link').fadeIn(520, "swing").css({"margin-left": x, "margin-top": y});
     }
   }, function() {
       setTimeout(function(){
         $('.share-link').fadeOut(830, "swing");
       }, 1000);
       setTimeout(function(){ myLinkPause = false; }, 30000);
+      setTimeout(function(){ hoverEngage = false; }, 1830);
   });
 
   // data table hover
 
   $('.data-table-hold').hover(function(e) {
-    if(myTablePause === false){
+    if(myTablePause === false && hoverEngage === false){
       var x = e.pageX - 70;
       var y = e.pageY - 70;
-      $('.table-time').fadeIn(520, "swing").css({"margin-left": x, "margin-top": y});
       myTablePause = true;
+      hoverEngage = true;
+      $('.table-time').fadeIn(520, "swing").css({"margin-left": x, "margin-top": y});
+
       setTimeout(function(){
         $('.table-time').fadeOut(830, "swing");
       }, 1520);
       setTimeout(function(){ myTablePause = false; }, 30000);
+      setTimeout(function(){ hoverEngage = false; }, 2350);
     }
   }, function() {
 
