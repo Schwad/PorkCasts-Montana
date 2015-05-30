6e7b85fa2a870e4ef236bffd8a1a65cb705058bc
Nick Schwaderer <nicholas.schwaderer@gmail.com>
2015-05-30 00:43:43 -0600

    creates hover dialogue on graphs and implements hover capabilities

--- app/assets/javascripts/show.js
+++ app/assets/javascripts/show.js
@@ -1,5 +1,23 @@
-$('#graph').hover(function() {
-    $('#graph-popup').show();
-}, function() {
-    $('#graph-popup').hide();
+var myGraphPause = false;
+
+$(document).ready(function(){
+
+  // graph hover
+  $('.graph-hold').hover(function(e) {
+    if(myGraphPause === false){
+      console.log("x " + e.pageX);
+      var x = e.pageX - 70;
+      var y = e.pageY - 70;
+      console.log("y " + e.pageY);
+        $('.graph-popup').fadeIn(520, "swing").css({"margin-left": x, "margin-top": y});
+      myGraphPause = true;
+    }
+  }, function() {
+      setTimeout(function(){
+        $('.graph-popup').fadeOut(830, "swing");
+      }, 1000);
+      setTimeout(function(){ myGraphPause = false; }, 14000);
+  });
+
+
 });
