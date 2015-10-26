26fe68810ba21dedfca99766c60738ad4eb0aec2
Nick Schwaderer <nicholas.schwaderer@gmail.com>
2015-10-26 11:32:52 +0000

    includes overall query overload method and helper on user

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,12 +16,24 @@ class User < ActiveRecord::Base
   end
 
   def active_queries
+    return self.queries.where("opt_out_email = ?", false).count
+  end
 
+  def queries_daily
+    return self.queries.where("created_at = ?", today).count
+  end
+
+  def active_queries_overload
+    if self.active_queries > 250
+      return true
+    else
+      return false
+    end
   end
 
   def queries_daily_overload
 
-    if self.queries.where("created_at = ?", today).count > 50
+    if self.queries_daily > 50
       return true
     else
       return false
