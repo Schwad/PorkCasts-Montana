05a3f204ce37430a584c7e8f36b9bd026e00d528
Nick Schwaderer <nicholas.schwaderer@gmail.com>
2015-10-27 16:55:53 +0000

    requires factory girl rails in rails helper

--- spec/factories/queries.rb
+++ spec/factories/queries.rb
@@ -2,5 +2,6 @@ FactoryGirl.define do
   factory :query do
     content "DANIEL ZOLNIKOV"
     association :user
+    user_id 1
   end
 end
--- spec/models/query_spec.rb
+++ spec/models/query_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 describe Query do
+  let(:query){build(:query)}
+
+  it "has a valid factory" do
+    expect(query).to be_valid
+  end
   it "is valid with content and a user_id"
   it "is invalid without a user id"
 
