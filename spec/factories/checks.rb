b57fdbf2c8b74ea98b7abbd9815690792114af3c
Nick Schwaderer <nicholas.schwaderer@gmail.com>
2015-10-29 14:26:29 +0000

    builds factories for checks and credit cards

---
+++ spec/factories/checks.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :credit_card do
+    amount 1234.0
+    department "something"
+    payee "someone"
+  end
+end
---
+++ spec/factories/creditcards.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :credit_card do
+    amount 1234.0
+    fiscal_year 2013
+    department "something"
+    merchant "someone"
+  end
+end
