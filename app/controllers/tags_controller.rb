32f21562f53b8880a8c5896f674e550a55953d5b
Nick Schwaderer <nicholas.schwaderer@gmail.com>
2015-10-30 13:36:39 +0000

    implements brakeman scanner

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,6 +1,7 @@
 class TagsController < ApplicationController
   def index
-     @tags = Rails.application.config.my_config["array"].grep(/#{params[:value].upcase}/)
+    myvalue = params[:value]
+     @tags = Rails.application.config.my_config["array"].grep(/#{myvalue.upcase}/)
 
     respond_to do |format|
       format.json { render :json => @tags.to_json }
