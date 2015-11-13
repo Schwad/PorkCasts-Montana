e4d3aeac42eefa382a17beb55825223690bfb150
Nick Schwaderer <nicholas.schwaderer@gmail.com>
2015-11-13 11:56:20 +0000

    new sets of twitter keys

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,6 +1,6 @@
   client = Twitter::REST::Client.new do |config|
-    config.consumer_key        = ""
-    config.consumer_secret     = ""
-    config.access_token        = ""
-    config.access_token_secret = ""
+    config.consumer_key        = "NoAoXM0rin3H4cAnvQ9QiryU7"
+    config.consumer_secret     = "gHb0ImsgApnwJB0oFweevYH744xiWeo3DE6v33EcrtXKAB4tz5"
+    config.access_token        = "20441925-beI6LAZ6SBe5q8qCxZjUMJV4uAPfy9ZE2pWLQuBDp"
+    config.access_token_secret = "Pb1AYeKZaLikgNznLRIfr8zLpXecYel87eDuFNODx1Pik"
   end
