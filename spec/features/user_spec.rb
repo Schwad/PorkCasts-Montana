468e56cecd417dc26e3c3ed89fbc0843cd601b47
Nick Schwaderer <nicholas.schwaderer@gmail.com>
2016-11-20 21:29:43 +0000

    adjusts specs for debugging

---
+++ spec/features/user_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+#support in /support/login_macros.rb
+
+feature 'User management' do
+  let(:user){create(:user)}
+
+  background do
+    # sign_in user
+  end
+
+  scenario 'logs in user' do
+    visit root_path
+    click_link "Sign In"
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+    save_and_open_page
+    click_button("Log in")
+    expect(page).to have_content('Queries')
+  end
+
+  scenario 'signs out user' do
+    # click_link "Sign out"
+    #
+    # expect(page).to have_content('Please login')
+  end
+end
