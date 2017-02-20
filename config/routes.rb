b0dff178c1a9556213b3dbe41efb02c44e238fc6
Nick Schwaderer <nicholas.schwaderer@gmail.com>
2017-02-20 14:52:43 +0000

    removes remarks about dashboard manifest

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    DashboardManifest::DASHBOARDS.each do |dashboard_resource|
-      resources dashboard_resource
-    end
 
-    root controller: DashboardManifest::ROOT_DASHBOARD, action: :index
-  end
 
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
