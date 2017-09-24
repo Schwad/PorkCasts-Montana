6ecb83205ffaf60a69620c00b538e758a8179cd5
Nick Schwaderer <nicholas.schwaderer@gmail.com>
2017-09-24 21:31:33 +0100

    marks wrapup point for weekend work

--- app/assets/javascripts/scroll_footer.js
+++ app/assets/javascripts/scroll_footer.js
@@ -6,8 +6,12 @@ var ScrollDetector = function()
       if( window.scrollY < 50 && $('body#landing-page').length != 1)
       {
           document.body.classList.remove( 'scrolled' );
+          var elem = document.getElementById("mainNav");
+          elem.style="background-color:transparent;border-bottom:none;"
       } else {
           document.body.classList.add( 'scrolled' );
+          var elem = document.getElementById("mainNav");
+          elem.style="background-color:black;border-bottom:none;"
       }
   });
 };
